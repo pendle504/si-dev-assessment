e307f532dd0e606a73b061b8b257911bedf4e537
Jared Shulman <jared@pendleton.ai>
2017-04-14 15:30:12 +0000

    Finishing Task 2&3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,5 @@ class PagesController < ApplicationController
   def home
   end
 
-  def sms
-    raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
-  end
+
 end
