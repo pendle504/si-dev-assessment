a7efa5ed566e027296d95183ddf4386acaa4937c
Jared Shulman <jared@pendleton.ai>
2017-04-14 21:28:27 +0000

    Finishing Task 6

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
+gem 'twilio-ruby'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
@@ -51,5 +51,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
+gem 'twilio-ruby'
 ruby "2.3.0"
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
+
   def home
   end
 
-
+  def sms
+    account_sid = "AC977749c87e41e5b93a8ca833a36baeba" 
+    auth_token = "3e09bfce702b24835af9aa85eba2f675" 
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.account.messages.create(:body => params[:text],
+        :to => '+1'+params[:number],    # Replace with your phone number
+        :from => "+15005550006")  # Replace with your Twilio number
+    puts message.sid
+  end
+  
+private
+  
+  def message_param
+    params.require(:message).permit(:text,:number)
+  end   
+  
 end
